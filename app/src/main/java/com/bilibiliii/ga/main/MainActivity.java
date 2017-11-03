d89119d6cabf8cc93d9ba1bd9e5c8a6120382a57
biao.ma@ninebot.com <jascal@163.com>
2017-11-03 11:36:00 +0800

    设置toolbar样式

--- app/src/main/java/com/bilibiliii/ga/main/MainActivity.java
+++ app/src/main/java/com/bilibiliii/ga/main/MainActivity.java
@@ -1,29 +1,59 @@
 package com.bilibiliii.ga.main;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.bilibiliii.ga.R;
+import com.bilibiliii.ga.base.BaseActivity;
 import com.bilibiliii.ga.bean.Event;
 import com.bilibiliii.ga.bean.Result;
 import com.bilibiliii.ga.utils.http.CallBack;
 import com.bilibiliii.ga.utils.http.CallBackException;
 import com.bilibiliii.ga.utils.http.RetrofitUtils;
 
-public class MainActivity extends AppCompatActivity implements CallBack<Event> {
+public class MainActivity extends BaseActivity implements CallBack<Event> {
     private final String TAG = getClass().getSimpleName();
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    }
 
-        try {
-            new RetrofitUtils().getHistoryEvent(this, "11", "3");
-        } catch (CallBackException e) {
-            e.printStackTrace();
-        }
+    @Override
+    protected void setContentView() {
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void initView() {
+        toolbar = ((Toolbar) findViewById(R.id.main_toolbar));
+        toolbar.setLogo(R.mipmap.ic_launcher);
+        toolbar.setTitle("Hello");
+        toolbar.setSubtitle("bupt");
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.mipmap.ic_launcher);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(toolbar, "Click setNavigationIcon", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void initPresenter() {
+
+    }
+
+    @Override
+    protected void initData() {
+
     }
 
     @Override
