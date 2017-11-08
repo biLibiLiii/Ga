5f1c78936a2dd27d0456d67ea18ce9a2614f3a99
biao.ma@ninebot.com <jascal@163.com>
2017-11-08 16:34:18 +0800

    add bmob callback

--- app/src/main/java/com/bilibiliii/ga/utils/bmob/CallBack.java
+++ app/src/main/java/com/bilibiliii/ga/utils/bmob/CallBack.java
@@ -1,8 +1,10 @@
 package com.bilibiliii.ga.utils.bmob;
 
+import com.bilibiliii.ga.utils.bmob.I.ICallback;
+
 /**
  * @author No.47 create at 2017/11/8.
  */
-public class CallBack {
+public class CallBack implements ICallback{
 
 }
---
+++ app/src/main/java/com/bilibiliii/ga/utils/bmob/I/ICallback.java
@@ -0,0 +1,7 @@
+package com.bilibiliii.ga.utils.bmob.I;
+
+/**
+ * @author No.47 create at 2017/11/8.
+ */
+public interface ICallback {
+}
