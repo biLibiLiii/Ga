c200036025d0fef5961b1a5961e8882afb8b825b
LiErGou <lichangliangsky@126.com>
2017-11-14 21:38:36 +0800

    登录注册

--- app/src/main/java/com/bilibiliii/ga/login/LoginActivity.java
+++ app/src/main/java/com/bilibiliii/ga/login/LoginActivity.java
@@ -1,14 +1,19 @@
 package com.bilibiliii.ga.login;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bilibiliii.ga.R;
 import com.bilibiliii.ga.base.BaseActivity;
 import com.bilibiliii.ga.bean.User;
+import com.bilibiliii.ga.main.MainActivity;
 import com.bilibiliii.ga.utils.bmob.CallBack;
 import com.bilibiliii.ga.utils.bmob.UserProxy;
 import com.bilibiliii.ga.view.RoundImageView;
@@ -22,6 +27,7 @@ public class LoginActivity extends BaseActivity {
     private Button mLoginButton;
     private Button mRegisteButton;
     private RoundImageView mRoundImageView;
+    private TextView mRegisteTextview;
     UserProxy mUserProxy;
 
     @Override
@@ -31,55 +37,55 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-        mUserProxy=UserProxy.getInstance();
+        Handler.Callback callback=new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message message) {
+                return false;
+            }
+        };
+        mUserProxy=UserProxy.getInstance(callback);
         mRoundImageView=(RoundImageView) findViewById(R.id.person_icon);
         mRoundImageView.setImageResource(R.drawable.icon_test);
         mUserNameEditText=(EditText)findViewById(R.id.username_edittext);
         mPassWordEditText=(EditText)findViewById(R.id.password_edittext);
         mLoginButton=(Button) findViewById(R.id.login_btn);
-        mRegisteButton=(Button)findViewById(R.id.registe_btn);
-        mRegisteButton.setOnClickListener(new View.OnClickListener() {
+        mRegisteTextview=(TextView)findViewById(R.id.regist_textview);
+        mRegisteTextview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("licl","you click register");
-                String userName=mUserNameEditText.getText().toString();
-                String passWord=mPassWordEditText.getText().toString();
-                String email=mUserNameEditText.getText().toString();
-                mUserProxy.register(userName, passWord, email, new CallBack<User>() {
-                    @Override
-                    public void onSuccess(User result) {
-                        Log.d("licl","register success");
-                        Toast.makeText(LoginActivity.this,"注册成功",Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onFail(String errorInfo) {
-                        Log.d("licl","register failed"+errorInfo);
-                        Toast.makeText(LoginActivity.this,"注册失败",Toast.LENGTH_SHORT).show();
-                    }
-                });
-
+                Intent intent=new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
             }
         });
+
 
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("licl","you click login");
+
                 String userName=mUserNameEditText.getText().toString();
                 String passWord=mPassWordEditText.getText().toString();
-                String email="";
+                if("".equals(userName)||"".equals(passWord)){
+                    Toast.makeText(LoginActivity.this,"用户名或密码不能为空",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Log.d("licl","you click login"+"userName:"+userName+" passWord:"+passWord);
                 mUserProxy.login(userName, passWord, new CallBack<User>() {
                     @Override
                     public void onSuccess(User result) {
                         Log.d("licl","login success");
+                        Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                        startActivity(intent);
                         Toast.makeText(LoginActivity.this,"登录成功",Toast.LENGTH_SHORT).show();
                     }
 
                     @Override
                     public void onFail(String errorInfo) {
                         Log.d("licl","login failed"+errorInfo);
-                        Toast.makeText(LoginActivity.this,"登录失败",Toast.LENGTH_SHORT).show();
+                        if("101".equals(errorInfo)){
+                            Toast.makeText(LoginActivity.this,"密码错误",Toast.LENGTH_SHORT).show();
+                        }
+
                     }
                 });
 
---
+++ app/src/main/java/com/bilibiliii/ga/login/RegisterActivity.java
@@ -0,0 +1,82 @@
+package com.bilibiliii.ga.login;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.bilibiliii.ga.R;
+import com.bilibiliii.ga.base.BaseActivity;
+import com.bilibiliii.ga.bean.User;
+import com.bilibiliii.ga.main.MainActivity;
+import com.bilibiliii.ga.utils.bmob.CallBack;
+import com.bilibiliii.ga.utils.bmob.UserProxy;
+
+public class RegisterActivity extends BaseActivity {
+    private EditText mUserNameEditText;
+    private Button mRegisteButton;
+    private EditText mPassWordEditText;
+    UserProxy mUserProxy;
+    @Override
+    protected void setContentView() {
+        setContentView(R.layout.activity_register);
+
+    }
+
+    @Override
+    protected void initView() {
+        mRegisteButton=(Button)findViewById(R.id.regist_btn);
+        mUserNameEditText=(EditText) findViewById(R.id.regist_username);
+        mPassWordEditText=(EditText)findViewById(R.id.regist_password);
+        mRegisteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userName=mUserNameEditText.getText().toString();
+                String passWord=mPassWordEditText.getText().toString();
+                if("".equals(userName)||"".equals(passWord)){
+                    Toast.makeText(RegisterActivity.this,"用户名或密码不能为空",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Handler.Callback callback=new Handler.Callback() {
+                    @Override
+                    public boolean handleMessage(Message message) {
+                        return false;
+                    }
+                };
+                Log.d("licl","you click login"+"userName:"+userName+" passWord:"+passWord);
+                mUserProxy= UserProxy.getInstance(callback);;
+                mUserProxy.register(userName, passWord, new CallBack<User>() {
+                    @Override
+                    public void onSuccess(User result) {
+                        Log.d("licl","register success");
+                        Toast.makeText(RegisterActivity.this,"注册成功",Toast.LENGTH_SHORT).show();
+                        Intent intent=new Intent(RegisterActivity.this,MainActivity.class);
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onFail(String errorInfo) {
+                        Log.d("licl","register failed"+errorInfo);
+                        Toast.makeText(RegisterActivity.this,"注册失败"+errorInfo,Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    protected void initPresenter() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
