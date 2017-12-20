44007bdd30861950daef593cc9ef84cb16e207a3
licl <lichangliangsky@126.com>
2017-12-20 11:24:55 +0800

    添加侧滑栏

--- app/src/main/java/com/bilibiliii/ga/main/MainActivity.java
+++ app/src/main/java/com/bilibiliii/ga/main/MainActivity.java
@@ -1,25 +1,45 @@
 package com.bilibiliii.ga.main;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bilibiliii.ga.R;
 import com.bilibiliii.ga.base.BaseActivity;
 import com.bilibiliii.ga.connector.ConnectorFragment;
 import com.bilibiliii.ga.conversation.ConversationListFragment;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends BaseActivity implements View.OnClickListener{
     private static final String TITLE="会话";
-    private TextView mTitle;
-    private ImageButton mBackImageButton;
-    private ImageButton mConverImageButton;
-    private ImageButton mConnectorImageButton;
-    private ImageButton mBrowserImageButton;
-    private ImageButton mRightImageButton;
+
+    @Bind(R.id.titlebar_title)
+    TextView mTitle;
+    @Bind(R.id.titlebar_left_imagebtn)
+    ImageButton mBackImageButton;
+    @Bind((R.id.conversation_imagebtn))
+    ImageButton mConverImageButton;
+    @Bind(R.id.connector_imagebtn)
+    ImageButton mConnectorImageButton;
+    @Bind(R.id.browser_imagebtn)
+    ImageButton mBrowserImageButton;
+    @Bind(R.id.titlebar_right_imagebtn)
+    ImageButton mRightImageButton;
     FragmentManager mFragmentManager;
     ConversationListFragment mConversationListFragment;
+    @Bind(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+    @Bind(R.id.navigationview)
+    NavigationView mNavigationView;
 
     @Override
     protected void setContentView() {
@@ -28,15 +48,11 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
 
     @Override
     protected void initView() {
-        mConverImageButton=(ImageButton) findViewById(R.id.conversation_imagebtn);
-        mConnectorImageButton=(ImageButton) findViewById(R.id.connector_imagebtn);
-        mBrowserImageButton=(ImageButton) findViewById(R.id.browser_imagebtn);
-        mTitle=(TextView) findViewById(R.id.titlebar_title);
-        mBackImageButton=(ImageButton)findViewById(R.id.titlebar_left_imagebtn);
-        mRightImageButton=(ImageButton)findViewById(R.id.titlebar_right_imagebtn);
+        ButterKnife.bind(this);
         mRightImageButton.setVisibility(View.VISIBLE);
         mRightImageButton.setBackgroundResource(R.drawable.comments);
         mBackImageButton.setVisibility(View.VISIBLE);
+        mNavigationView.setItemIconTintList(null);
         mTitle.setText(TITLE);
         mBackImageButton.setBackgroundResource(R.drawable.category);
         mConversationListFragment=new ConversationListFragment();
@@ -47,6 +63,22 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
         mConverImageButton.setOnClickListener(this);
         mConnectorImageButton.setOnClickListener(this);
         mBrowserImageButton.setOnClickListener(this);
+        mBackImageButton.setOnClickListener(this);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId=item.getItemId();
+                switch (itemId){
+                    case R.id.weather:
+                        Toast.makeText(MainActivity.this,"click weather",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.location:
+                        Toast.makeText(MainActivity.this,"click location",Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
     @Override
@@ -85,6 +117,8 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
                 resetImageIcon();
                 mBrowserImageButton.setBackgroundResource(R.drawable.browse_fill);
                 break;
+            case R.id.titlebar_left_imagebtn:
+                mDrawerLayout.openDrawer(mNavigationView);
         }
     }
 
